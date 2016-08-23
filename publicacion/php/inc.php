5d76ef36c27faa32075c8177f739758ba0a589c1
Oliver Castelblanco Martínez <ocastelblanco@gmail.com>
2016-08-22 19:11:17 -0500

    Claves DB online

--- publicacion/php/inc.php
+++ publicacion/php/inc.php
@@ -1,18 +1,20 @@
 <?php
 date_default_timezone_set('America/Bogota');
-$SERVIDOR = "localhost";
+/**/
 $USUARIO = "adminCursos";
 $CLAVE = "millonarios";
 $DB = "adminCursos";
+/**/
+/*
+$USUARIO = "eb000384_cursos";
+$CLAVE = "S31nTS4S";
+$DB = "eb000384_cursos";
+*/
+$SERVIDOR = "localhost";
 $TABLA_USUARIOS = "usuarios";
 $TABLA_CURSOS = "listadocursos";
 $TABLA_REGISTRO = "cursos";
 $TABLA['usuarios'] = $TABLA_USUARIOS;
 $TABLA['cursos'] = $TABLA_CURSOS;
 $TABLA['registros'] = $TABLA_REGISTRO;
-/*
-$USUARIO = "eb000384_cursos";
-$CLAVE = "S31nTS4S";
-$DB = "eb000384_cursos";
-*/
 ?>
